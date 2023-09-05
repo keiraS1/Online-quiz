378ccd51677b555ce88a88e13341c0d85e5e9668
keiraS1 <ktsamant0411@gmail.com>
2023-09-05 18:27:29 -0400

    fixed error in code created new JS file so quiz would start

---
+++ JS/highscores.js
@@ -0,0 +1,29 @@
+    // high score page
+// } else {
+
+    //when you submit the name form
+    document.getElementById('scoreForm').addEventListener('submit', event => {
+        event.preventDefault();
+
+        const name = document.getElementById('name').value;
+        const plays = JSON.parse(localStorage.getItem('plays')) || []; //pulls plays Array outside of the localstorage
+        plays.push({ name, score: localStorage.getItem('score') });
+        localStorage.setItem('plays', JSON.stringify(plays)); //saves updated plays array in the local storage
+
+        showPlays(); // after saving a new name/score
+
+        document.getElementById('name').remove();
+        document.getElementById('submitButton').remove();
+    });
+
+    function showPlays() {
+        document.getElementById('leaderboard').innerHTML = '';
+        const plays = JSON.parse(localStorage.getItem('plays')) || [];
+        for (let i = 0; i < plays.length; i++) {
+            const currentPlay = plays[i];
+            const listElement = document.createElement('li');
+            listElement.innerText = currentPlay.name + ' - ' + currentPlay.score
+            document.getElementById('leaderboard').append(listElement);
+        }
+    }
+    showPlays();   // the past plays 
--- JS/script.js
+++ JS/script.js
@@ -1,5 +1,5 @@
 // index.html page
-if (window.location.pathname === "/Online-quiz/index.html") {
+// if (window.location.pathname === "/Online-quiz/index.html") {
     localStorage.removeItem('score'); //this will prevent the last score from adding onto the next players score 
     let score = 0;
     var questionEl = document.getElementById("question");
@@ -142,33 +142,3 @@ if (window.location.pathname === "/Online-quiz/index.html") {
     }
 
 
-    // high score page
-} else {
-
-    //when you submit the name form
-    document.getElementById('scoreForm').addEventListener('submit', event => {
-        event.preventDefault();
-
-        const name = document.getElementById('name').value;
-        const plays = JSON.parse(localStorage.getItem('plays')) || []; //pulls plays Array outside of the localstorage
-        plays.push({ name, score: localStorage.getItem('score') });
-        localStorage.setItem('plays', JSON.stringify(plays)); //saves updated plays array in the local storage
-
-        showPlays(); // after saving a new name/score
-
-        document.getElementById('name').remove();
-        document.getElementById('submitButton').remove();
-    });
-
-    function showPlays() {
-        document.getElementById('leaderboard').innerHTML = '';
-        const plays = JSON.parse(localStorage.getItem('plays')) || [];
-        for (let i = 0; i < plays.length; i++) {
-            const currentPlay = plays[i];
-            const listElement = document.createElement('li');
-            listElement.innerText = currentPlay.name + ' - ' + currentPlay.score
-            document.getElementById('leaderboard').append(listElement);
-        }
-    }
-    showPlays();   // the past plays 
-}
